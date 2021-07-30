2db1b87697edcc8c57acc9e146a67e04668915bd
Diego Anibal Dalairac <ddalairac@perceptyx.com>
2021-07-30 11:28:21 -0300

    some progress

---
+++ pruebas/microfront-v2/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p>params: {{ paramsString }}</p>
+    <p>params: {{ params }}</p>
+    <p>client: {{ client }}</p>
+    <p>survey: {{ survey }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    getParams() {
+      var params = {};
+      var url_parts = window.location.search.split("?");
+      if (url_parts.length === 2) {
+        var param_parts = url_parts[1].split("&");
+        for (var i = 0; i < param_parts.length; i++) {
+          var kv = param_parts[i].split("=");
+          params[kv[0]] = kv[1];
+        }
+      }
+      console.log("params", params);
+      return params;
+    },
+  },
+  computed: {
+    paramsString() {
+      console.log("computed");
+      return window.location.search;
+    },
+    params() {
+      var params = {};
+      var url_parts = window.location.search.split("?");
+      if (url_parts.length === 2) {
+        var param_parts = url_parts[1].split("&");
+        for (var i = 0; i < param_parts.length; i++) {
+          var kv = param_parts[i].split("=");
+          params[kv[0]] = kv[1];
+        }
+      }
+      console.log("params", params);
+      return params;
+    },
+    client() {
+      return "sd"//getParams().client ? params.client : null;
+    },
+    survey() {
+        return "sd"//getParams().survey ? params.survey : null;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
